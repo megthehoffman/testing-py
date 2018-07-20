83257806c8140e1e3b29679de6e48e58b1429b83
Hackbright Student <no-reply@hackbrightacademy.com>
2018-07-19 18:08:41 -0700

    Completed all tests, tests pass

--- tests.py
+++ tests.py
@@ -22,8 +22,9 @@ class PartyTests(unittest.TestCase):
     def test_no_rsvp_yet(self):
         """Do users who haven't RSVPed see the correct view?"""
 
-        # FIXME: Add a test to show we haven't RSVP'd yet
-        print("FIXME")
+        result = self.client.get("/")
+        self.assertIn(b"<h2>Please RSVP</h2>", result.data)
+        self.assertNotIn(b"<h2>Party Details</h2>", result.data)
 
     def test_rsvp(self):
         """Do RSVPed users see the correct view?"""
@@ -32,17 +33,14 @@ class PartyTests(unittest.TestCase):
 
         result = self.client.post("/rsvp", data=rsvp_info,
                                   follow_redirects=True)
-
-        # FIXME: check that once we log in we see party details--but not the form!
-        print("FIXME")
+        self.assertNotIn(b"<<h2>Please RSVP</h2>", result.data)
+        self.assertIn(b"<h2>Party Details</h2>", result.data)
 
     def test_rsvp_mel(self):
         """Can we keep Mel out?"""
 
-        # FIXME: write a test that mel can't invite himself
-        pass
-        print("FIXME")
-
+        result = self.client.post("/rsvp", data={"field-name" : "Mel Melitpolski", "field-email" : "mel@ubermelon.com"})
+        self.assertNotIn(b"<h2>Party Details</h2>", result.data)
 
 if __name__ == "__main__":
     unittest.main()
